7b0b302a7dc2a3b81ca7e2c7a4dda718e9c45a71
Carl-Philip Hänsch <carli@launix.de>
2012-11-14 09:44:27 +0100

    Strenge Vergleiche

--- jsontype.js
+++ jsontype.js
@@ -3,21 +3,21 @@ Module jsontype.js
 
 */
 
-makeValidator = function(type) {
+var makeValidator = function(type) {
 	var regex_result;
 
 	// Input: string
 	if(/^\s*string\s*$/.test(type)) {
 		// type is string
 		return function(value) {
-			return typeof(value) == 'string';
-		}
+			return typeof(value) === 'string';
+		};
 	}
 	if(/^\s*number\s*$/.test(type)) {
 		// type is number
 		return function(value) {
-			return typeof(value) == 'number';
-		}
+			return typeof(value) === 'number';
+		};
 	}
 	if(/^\s*boolean\s*$/.test(type)) {
 		// type is boolean
@@ -28,35 +28,35 @@ makeValidator = function(type) {
 	if(/^\s*undefined\s*$/.test(type)) {
 		// check for undefined
 		return function(value) {
-			return typeof(value) == 'undefined';
-		}
+			return typeof(value) === 'undefined';
+		};
 	}
 	if(/^\s*\*\s*$/.test(type)) {
 		// any-Match (everything but undefined)
 		return function(value) {
-			return value != undefined;
-		}
+			return value !== undefined;
+		};
 	}
 	if((regex_result = /^\s*"(.*)"\s*$/.exec(type)) || (regex_result = /^\s*"(.*)"\s*$/.exec(type))) {
 		// match constant string
 		var xstring = regex_result[1];
 		return function(value) {
-			return value == xstring;
-		}
+			return value === xstring;
+		};
 	}
-	if(regex_result = /^\s*\[(.+)\]\s*$/.exec(type)) {
+	if((regex_result = /^\s*\[(.+)\]\s*$/.exec(type))) {
 		// array-of-match
 		var subtype = makeValidator(regex_result[1]);
 		return function(value) {
-			if(typeof(value) != 'object') return false;
-			if(value.constructor != Array) return false;
+			if(typeof(value) !== 'object') return false;
+			if(value.constructor !== Array) return false;
 			for(var i in value) {
 				if(!subtype(value[i])) return false;
 			}
 			return true;
-		}
+		};
 	}
-	if(regex_result = /^\s*\{(.*)\}\s*$/.exec(type)) {
+	if((regex_result = /^\s*\{(.*)\}\s*$/.exec(type))) {
 		// objects
 		var content = regex_result[1];
 		var types = {}; // associative list of type restrictions
@@ -77,38 +77,38 @@ makeValidator = function(type) {
 			var rest = r[2]; // type, optional comma and rest
 			var brstack = []; // stack to count brackets
 			var i = 0;
-			while(i < rest.length && (rest.charAt(i) != ',' || brstack.length > 0)) {
+			while(i < rest.length && (rest.charAt(i) !== ',' || brstack.length > 0)) {
 				switch(rest.charAt(i)) {
 					case "{": brstack.push('}'); break;
 					case "(": brstack.push(')'); break;
 					case "[": brstack.push(']'); break;
 					case '}': case ')': case ']':
-					if(brstack.length == 0) throw('closing bracket without opening in: '+rest);
+					if(brstack.length === 0) throw('closing bracket without opening in: '+rest);
 					var last = brstack.pop();
-					if(rest.charAt(i) != last)
+					if(rest.charAt(i) !== last)
 						throw "Expected "+last+' but found '+rest.charAt(i)+' in: '+rest;
 					break;
 					// TODO: also test for fixed strings?
 				}
-				i++
+				i++;
 			}
 			var fn = makeValidator(rest.slice(0, i));
 			if(optional) (function(fn){
 				// also check optional fields
 				types[ident] = function(value) {
-					if(value == undefined) return true;
+					if(value === undefined) return true;
 					return fn(value); // type must either be correct or undefined
-				}
+				};
 			})(fn);
 			else types[ident] = fn;
 			content = rest.slice(i+1); // everything behind the comma
 		}
 		return function(value) {
 			// validate complex object structures
-			if(typeof(value) != 'object') return false;
+			if(typeof(value) !== 'object') return false;
 			// for all definitions:
 			for(var i in types) {
-				if(i == '*') {
+				if(i === '*') {
 					// whitelist-check
 					for(var j in value) {
 						// a unknown property which does not fit the wanted *-type
@@ -119,11 +119,11 @@ makeValidator = function(type) {
 				}
 			}
 			return true;
-		}
+		};
 	}
 	// TODO: check for OR
 	throw "Unknown type identifier: "+type;
-}
+};
 
 
 exports.makeValidator = makeValidator;
@@ -131,5 +131,5 @@ exports.makeThrowValidator = function(type) {
 	var vali = makeValidator(type);
 	return function(value) {
 		if(!vali(value)) throw "data format not matched";
-	}
-}
+	};
+};
