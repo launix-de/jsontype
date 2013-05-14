ab5d97a205155d3bf81355a7abdf135b86e42c98
Carl-Philip <s3734770@mail.zih.tu-dresden.de>
2013-05-14 11:02:36 +0200

    Initial Commit

--- jsontype.js
+++ jsontype.js
@@ -3,91 +3,141 @@ Module jsontype.js
 
 */
 
-makeValidator = function(type) {
+// {type: string, additionaltypes: {*: function}
+var makeValidator = function(type, additionaltypes) {
 	var regex_result;
 
 	// Input: string
 	if(/^\s*string\s*$/.test(type)) {
 		// type is string
 		return function(value) {
-			return typeof(value) == 'string';
-		}
+			return typeof(value) === 'string';
+		};
+	}
+	if(/^\s*mail\s*$/.test(type)) {
+		// type is email
+		return function(value) {
+			return typeof(value) === 'string' && /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value);
+		};
 	}
 	if(/^\s*number\s*$/.test(type)) {
 		// type is number
 		return function(value) {
-			return typeof(value) == 'number';
+			return typeof(value) === 'number';
+		};
+	}
+	if(/^\s*boolean\s*$/.test(type)) {
+		// type is boolean
+		return function(value) {
+			return typeof(value) == 'boolean';
+		}
+	}
+	if(/^\s*function\s*$/.test(type)) {
+		// type is boolean
+		return function(value) {
+			return typeof(value) == 'function';
 		}
 	}
 	if(/^\s*undefined\s*$/.test(type)) {
 		// check for undefined
 		return function(value) {
-			return typeof(value) == 'undefined';
-		}
+			return typeof(value) === 'undefined';
+		};
 	}
 	if(/^\s*\*\s*$/.test(type)) {
 		// any-Match (everything but undefined)
 		return function(value) {
-			return value != undefined;
+			return value !== undefined;
+		};
+	}
+	if(additionaltypes) {
+		// Match von nutzerdefinierten Namen
+		regex_result = /^\s*(.*?)\s*$/.exec(type);
+		if(regex_result && additionaltypes.hasOwnProperty(regex_result[1])) {
+			var fn = additionaltypes[regex_result[1]];
+			return function(value) {
+				try {
+					return fn(value);
+				} catch(x) {
+					return false;
+				}
+			};
 		}
 	}
-	if(regex_result = /^\s*\[(.+)\]\s*$/.exec(type)) {
-		// array-of-match
-		var subtype = makeValidator(regex_result[1]);
+
+	if((regex_result = /^\s*"(.*)"\s*$/.exec(type)) || (regex_result = /^\s*"(.*)"\s*$/.exec(type))) {
+		// match constant string
+		var xstring = regex_result[1];
 		return function(value) {
-			if(typeof(value) != 'object') return false;
-			if(value.constructor != Array) return false;
+			return value === xstring;
+		};
+	}
+	if((regex_result = /^\s*\[(.+)\]\s*$/.exec(type))) {
+		// array-of-match
+		var subtype = makeValidator(regex_result[1], additionaltypes);
+		return function(value) {
+			if(typeof(value) !== 'object') return false;
+			if(value.constructor !== Array) return false;
 			for(var i in value) {
 				if(!subtype(value[i])) return false;
 			}
 			return true;
-		}
+		};
 	}
-	if(regex_result = /^\s*\{(.*)\}\s*$/.exec(type)) {
+	if((regex_result = /^\s*\{(.*)\}\s*$/.exec(type))) {
 		// objects
 		var content = regex_result[1];
 		var types = {}; // associative list of type restrictions
 		while(!/^\s*$/.test(content)) {
-			var r = /^\s*(\w*\??|\*)\s*:(.*)$/.exec(content);
+			var optional = false;
+			var r = /^\s*(\w+)\?\s*:(.*)$/.exec(content);
+			if(!r) r = /^\s*"(.+?)"\?\s*:(.*)$/.exec(content);
+			if(!r) r = /^\s*'(.+?)'\?\s*:(.*)$/.exec(content);
+			if(r) optional = true;
+			else {
+				if(!r) r = /^\s*"(.+?)"\s*:(.*)$/.exec(content);
+				if(!r) r = /^\s*'(.+?)'\s*:(.*)$/.exec(content);
+				// TODO: do better parsing of " escape
+				if(!r) r = /^\s*(\w+|\*)\s*:(.*)$/.exec(content);
+			}
 			if(!r) throw "Error in object type syntax near: "+content;
 			var ident = r[1]; // Identifier of the property
 			var rest = r[2]; // type, optional comma and rest
 			var brstack = []; // stack to count brackets
 			var i = 0;
-			while(i < rest.length && (rest.charAt(i) != ',' || brstack.length > 0)) {
+			while(i < rest.length && (rest.charAt(i) !== ',' || brstack.length > 0)) {
 				switch(rest.charAt(i)) {
 					case "{": brstack.push('}'); break;
 					case "(": brstack.push(')'); break;
 					case "[": brstack.push(']'); break;
 					case '}': case ')': case ']':
-					if(brstack.length == 0) throw('closing bracket without opening in: '+rest);
+					if(brstack.length === 0) throw('closing bracket without opening in: '+rest);
 					var last = brstack.pop();
-					if(rest.charAt(i) != last)
+					if(rest.charAt(i) !== last)
 						throw "Expected "+last+' but found '+rest.charAt(i)+' in: '+rest;
 					break;
 					// TODO: also test for fixed strings?
 				}
-				i++
+				i++;
 			}
-			var fn = makeValidator(rest.slice(0, i));
-			r = /^(\w*)\?$/.exec(ident);
-			if(r) (function(fn){
-				ident = r[1];
+			var fn = makeValidator(rest.slice(0, i), additionaltypes);
+			if(optional) (function(fn){
 				// also check optional fields
-				if(r) types[ident] = function(value) {
-					if(value == undefined) return true;
+				types[ident] = function(value) {
+					if(value === undefined) return true;
 					return fn(value); // type must either be correct or undefined
-				}
+				};
 			})(fn);
 			else types[ident] = fn;
 			content = rest.slice(i+1); // everything behind the comma
 		}
 		return function(value) {
+			if(!value) return false;
 			// validate complex object structures
-			if(typeof(value) != 'object') return false;
+			if(typeof(value) !== 'object') return false;
 			// for all definitions:
 			for(var i in types) {
-				if(i == '*') {
+				if(i === '*') {
 					// whitelist-check
 					for(var j in value) {
 						// a unknown property which does not fit the wanted *-type
@@ -98,11 +148,17 @@ makeValidator = function(type) {
 				}
 			}
 			return true;
-		}
+		};
 	}
 	// TODO: check for OR
 	throw "Unknown type identifier: "+type;
-}
+};
 
 
 exports.makeValidator = makeValidator;
+exports.makeThrowValidator = function(type, additionaltypes) {
+	var vali = makeValidator(type, additionaltypes);
+	return function(value) {
+		if(!vali(value)) throw "data format not matched";
+	};
+};
--- runtests.js
+++ runtests.js
@@ -16,10 +16,30 @@ test(stringvalidator('abc'));
 test(!stringvalidator(123));
 test(!stringvalidator({}));
 
+var mailvalidator = makeValidator('	mail\t');
+test(mailvalidator('carlo@gmx.net'));
+test(mailvalidator('carlo.kater@gmx.net'));
+test(mailvalidator('carlo-kater@gmx.net'));
+test(mailvalidator('carlo@gmx.net.to'));
+test(!mailvalidator('@gmx.net'));
+test(!mailvalidator('carlo.net'));
+test(!mailvalidator('carlo@gmx'));
+
 var numbervalidator = makeValidator('number   	');
 test(numbervalidator(3));
 test(!numbervalidator('4'));
 test(!numbervalidator(function(){}));
+
+var booleanvalidator = makeValidator(' boolean   	');
+test(booleanvalidator(true));
+test(booleanvalidator(false));
+test(!booleanvalidator(0));
+
+var functionvalidator = makeValidator('function');
+test(functionvalidator(function(){}));
+test(functionvalidator(function(a, b){return 3}));
+test(functionvalidator(Object.toString));
+test(!functionvalidator({}));
 
 var anyvalidator = makeValidator('*');
 test(anyvalidator(6));
@@ -47,10 +67,20 @@ test(!makeValidator('{*:undefined}')({a: 3}));
 test(makeValidator('{a: number, *:undefined}')({a: 3}));
 
 test(makeValidator('{a: number, b: string}')({b: 'd', a: 5.7}));
-var complexvalidator = makeValidator('{a: {x: number, y: number}, b: {*: undefined}}');
-test(complexvalidator({a: {x: 3, y: 6}, b: {}}));
-test(!complexvalidator({a: {x: 3, z: 6}, b: {}}));
-test(!complexvalidator({a: {x: 3, y: 6}, b: {b: 6}}));
+
+var complexvalidator = makeValidator('{a: {"x": number, \'y\': number}, b: {*: undefined}, \'c\': foo, d?: bar}', 
+       {
+               foo: function(value){return stringvalidator(value) && /foo/.test(value)},
+               bar: function(value){return false}
+       });
+test(complexvalidator({a: {x: 3, y: 6}, b: {}, c: 'foo'}));
+test(!complexvalidator({a: {x: 3, z: 6}, b: {}, c: 'foo'}));
+test(!complexvalidator({a: {x: 3, y: 6}, b: {b: 6}, c: 'foo'}));
+test(complexvalidator({a: {x: 3, y: 6}, b: {}, c: 'lal foo foo x'}));
+test(!complexvalidator({a: {x: 3, y: 6}, b: {}, c: 'foo', d: 4}));
+test(!complexvalidator({a: {x: 3, y: 6}, b: {}, c: 'bar'}));
+test(!complexvalidator({a: {x: 3, y: 6}, b: {}, c: {foo: 'foo'}}));
+
 
 var optionalvalidator = makeValidator('{muss: number, kann?: number, *: undefined}');
 test(optionalvalidator({muss: 1, kann: 4}));
@@ -61,6 +91,31 @@ test(!optionalvalidator({kann: 4}));
 test(!optionalvalidator({}));
 test(!optionalvalidator(2));
 test(!optionalvalidator([1, 2, 3]));
+
+test(!makeValidator('{*: undefined}')({x: 2}));
+test(makeValidator('{"a": string}')({a: 'test'}));
+
+
+var fixedstringvalidator = makeValidator('{"action": "fire", par: number}');
+test(fixedstringvalidator({action: 'fire', par: 2}));
+test(!fixedstringvalidator({action: 'stop', par: 2}));
+test(!fixedstringvalidator({action: 'fire'}));
+test(!fixedstringvalidator({action: 'fire', par: 'a'}));
+test(!fixedstringvalidator({action: {}, par: 2}));
+test(!fixedstringvalidator({par: 2}));
+
+test(makeValidator('{"action": "fire", "par": number, "a": "b"}')({action: "fire", par: 3, a: "b"}));
+
+var recursiveValidators = {fixedstr: fixedstringvalidator, optional: optionalvalidator};
+var extravalidator = makeValidator('{a: number, b?: fixedstr, c: optional}', recursiveValidators);
+test(extravalidator({a: 3, b: {action: 'fire', par: 2}, c: {muss: 1, kann: 4}}));
+test(extravalidator({a: 3, c: {muss: 1, kann: 4}}));
+test(!extravalidator({a: function(){}, b: {action: 'fire', par: 2}, c: {muss: 1, kann: 4}}));
+test(extravalidator({a: 3, b: {action: 'fire', par: 2}, c: {muss: 1}}));
+test(!extravalidator({a: '3', b: {action: 'fire', par: 2}, c: {muss: 1, kann: 4}}));
+test(!extravalidator({a: 3, b: {action: 'fire2', par: 2}, c: {muss: 1, kann: 4}}));
+test(!extravalidator({a: 3}));
+test(!extravalidator(null));
 
 
 console.log('Tests: '+testPositive+'/'+testSum);
